6f15b9286baca5eafe4e9dfe5cbc83919f31e98e
turdaliyev-art <bturdaliyev49@gmail.com>
2026-04-02 17:33:29 +0500

    .

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+import styles from "./Header.module.css"
+
+function Header() {
+  return (
+    <header>
+        <div className={styles.search}>
+            <IoSearch  className={styles.searchIcon}/>
+             <input type="search" placeholder='Search transactions, assets, or users...'/> 
+
+        </div>
+
+        <div className={styles.userProfile}>
+            <div className={styles.aboutUser}>
+                <p className={styles.firstname}>Alexander Vance</p>
+                <p className={styles.userLevel}>Admin Access</p>
+            </div>
+            <img src="user.png" alt="user.png" />
+        </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styles from "./Sidebar.module.css"
+import { NavLink } from 'react-router-dom'
+
+function Sidebar() {
+  return (
+    <div className={styles.sidebarContainer}>
+      <div className={styles.pageName}>
+        <img src="./sidebarLogo.png" alt="" />
+        <div className={styles.sidebarDesc}>
+            <p className={styles.pageTitle}>Fiscal Atelier</p>
+            <p className={styles.pageDesc}>Wealth Management</p>
+        </div>
+      </div>
+      
+        <ul>
+            <li className={`${styles.active}`}>  <img src="./sidebarDashboard.png" alt="sidebarDashboard.png" className='icon'/> <a className={`${styles.active}`} href='/'>Dashboard</a></li>
+            <li> <img src="./Transactions.svg" alt="Transactions.png"/> <a href='/'>Transactions</a></li>
+            <li> <img src="./UserList.svg" alt="Transactions.png" className='icon'/> <a href='/'>User List</a></li>
+        </ul>
+
+        
+    
+    </div>
+  )
+}   
+
+export default Sidebar
